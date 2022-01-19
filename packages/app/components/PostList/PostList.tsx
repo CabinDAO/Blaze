ebf4a8e27bfe89d0430871271875e5adbd8aeb12
Matthew Watman <mattwatman@gmail.com>
2022-01-19 15:05:25 -0500

    feat(dropdown): Post list data functionality

--- packages/app/components/PostList/PostList.tsx
+++ packages/app/components/PostList/PostList.tsx
@@ -1,21 +1,23 @@
 import { styled } from "@/stitches.config";
 import Post from "../Post";
+import {v4 as uuidV4} from "uuid";
+import { PostListProps } from "@/types";
 
-interface PostListProps {
-    posts: {title: string}[];
-    sort: "newest"|"trending";
-}
+
 const StyledPostList = styled("div", {
-    display: "flex",
-    flexDirection: "column",
-    rowGap: "$4",
-  });
+  display: "flex",
+  flexDirection: "column",
+  rowGap: "$4",
+});
 
-const PostList = ({posts, sort}: PostListProps) => {
-
-  return <StyledPostList>
-
-  </StyledPostList>;
+const PostList = ({ posts, sort }: PostListProps) => {
+  return (
+    <StyledPostList>
+      {sort === "newest"
+        ? posts.sort((a, b) => a.submissionDate - b.submissionDate).map((post) => <Post key={uuidV4()} {...post} />)
+        : posts.map((post) => <Post key={uuidV4()} {...post} />)}
+    </StyledPostList>
+  );
 };
 
 export default PostList;
