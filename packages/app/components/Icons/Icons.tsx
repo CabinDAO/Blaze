1918abe57f04307356e1d7fbcb1a1555021dbcbb
Matthew Watman <mattwatman@gmail.com>
2022-01-26 11:58:12 -0500

    fix(icons): update calendar, clock and external link icons

--- packages/app/components/Icons/Icons.tsx
+++ packages/app/components/Icons/Icons.tsx
@@ -14,9 +14,13 @@ export const ClockIcon = ({ className }: { className?: string }) => (
   </svg>
 );
 
-export const SpeechIcon = (
-  { className, fill }: { className?: string, fill: boolean },
-) => (
+export const SpeechIcon = ({
+  className,
+  fill,
+}: {
+  className?: string;
+  fill: boolean;
+}) => (
   <svg
     className={className}
     width="12"
@@ -31,4 +35,44 @@ export const SpeechIcon = (
       stroke="#324841"
     />
   </svg>
+);
+
+export const CalendarIcon = ({ className }: { className?: string }) => (
+  <svg
+    className={className}
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M9 8.5H8.5V9V11.5H0.5V4H11.5V8.5H9ZM9.5 11.5V9.5H11.5V11.5H9.5ZM11.5 3H0.5V0.5H11.5V3Z"
+      stroke="#324841"
+    />
+  </svg>
+);
+
+export const ExternalLinkIcon = ({
+  className,
+  href,
+}: {
+  className?: string;
+  href: string;
+}) => (
+    <a href={href} target="_blank" rel="noreferrer">
+      <svg
+        className={className}
+        width="11"
+        height="11"
+        viewBox="0 0 11 11"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M10.5 0.5L4 7M10.5 0.5H7M10.5 0.5V4M5 0.5H0.5L0.5 10.5H10.5V6"
+          stroke="#324841"
+        />
+      </svg>
+    </a>
 );
