68527663589810fa37ab58490ec78f5b0563d2db
Matthew Watman <mattwatman@gmail.com>
2022-01-28 11:39:19 -0500

    feat(back-end): add dependencies for retrieving mirror feeds

--- packages/app/pages/api/cron.ts
+++ packages/app/pages/api/cron.ts
@@ -1,5 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { parse } from "rss-to-json";
+import OrbitDB from "orbit-db";
+import Ipfs from "ipfs";
+import DaoCampDB from "../../scripts/db";
 
+//Mirror feeds to seed database
+// Cabin, Krause House, MirrorDAO, Songcamp, Seed Club
+const MirrorFeedURLs = ["https://dao.mirror.xyz/OvXLCC3v2hUR06ay0g6wvn3zH1mShq4z3VCfto6TBKY, "]
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -9,6 +16,7 @@ export default async function handler(
       const { authorization } = req.headers;
 
       if (authorization === `Bearer ${process.env.API_SECRET_KEY}`) {
+
         res.status(200).json({ success: true });
       } else {
         res.status(401).json({ success: false });
--- packages/app/scripts/db.ts
+++ packages/app/scripts/db.ts
@@ -1,5 +1,5 @@
-const { v4: uuidv4 } = require('uuid');
-const { getUnixTime } = require('date-fns'); 
+const { v4: uuidv4 } = require("uuid");
+const { getUnixTime } = require("date-fns");
 
 export default class DaoCampDb {
   constructor(Ipfs, OrbitDB) {
@@ -9,27 +9,30 @@ export default class DaoCampDb {
   async create() {
     this.node = await this.Ipfs.create({
       preload: { enabled: false },
-      repo: './ipfs',
+      repo: "./ipfs",
       EXPERIMENTAL: { pubsub: true },
       config: {
         Bootstrap: [],
-        Addresses: { Swarm: [] }
-      }
-    })
+        Addresses: { Swarm: [] },
+      },
+    });
 
-    this._init()
+    this._init();
   }
 
   async _init() {
-    this.orbitdb = await this.OrbitDB.createInstance(this.node)
+    this.orbitdb = await this.OrbitDB.createInstance(this.node);
     this.defaultOptions = {
       accessController: {
-        write: [this.orbitdb.identity.id]
-      }
-    }
-    this.profiles = await this.orbitdb.docstore('profiles', this.defaultOptions)
-    this.links = await this.orbitdb.docstore('links', this.defaultOptions)
-    this.upvotes = await this.orbitdb.docstore('upvotes', this.defaultOptions)
+        write: [this.orbitdb.identity.id],
+      },
+    };
+    this.profiles = await this.orbitdb.docstore(
+      "profiles",
+      this.defaultOptions
+    );
+    this.links = await this.orbitdb.docstore("links", this.defaultOptions);
+    this.upvotes = await this.orbitdb.docstore("upvotes", this.defaultOptions);
     await this.profiles.load();
     await this.links.load();
     await this.upvotes.load();
@@ -40,10 +43,14 @@ export default class DaoCampDb {
     const upvotesStoreName = `${walletAddress}-upvotes-received`;
     const upvotedStoreName = `${walletAddress}-links-upvoted`;
 
-    const counter1 = await this.orbitdb.counter(upvotesStoreName, this.defaultOptions);
-    const counter2 = await this.orbitdb.counter(upvotedStoreName, this.defaultOptions);
-
-
+    const counter1 = await this.orbitdb.counter(
+      upvotesStoreName,
+      this.defaultOptions
+    );
+    const counter2 = await this.orbitdb.counter(
+      upvotedStoreName,
+      this.defaultOptions
+    );
 
     const profile = {
       _id: walletAddress,
@@ -53,43 +60,52 @@ export default class DaoCampDb {
       upvotesReceived: counter1.id,
       linksUpvoted: counter2.id,
     };
-    const cid = await this.profiles.put(profile)
+    const cid = await this.profiles.put(profile);
     return cid;
   }
 
   async updateLastSeenDate(walletAddress) {
     const profile = await this.profiles.get(walletAddress);
-    const cid = await this.profiles.put(walletAddress, { ...profile, lastSeenDate: getUnixTime(new Date()) });
-    return cid
+    const cid = await this.profiles.put(walletAddress, {
+      ...profile,
+      lastSeenDate: getUnixTime(new Date()),
+    });
+    return cid;
   }
 
   async addNewLink(title, url) {
     const _id = uuidv4();
     const upvotesStoreName = `${id}-link-upvotes-received`;
-    const counter = await this.orbitdb.counter(upvotesStoreName, this.defaultOptions);
+    const counter = await this.orbitdb.counter(
+      upvotesStoreName,
+      this.defaultOptions
+    );
     const link = {
       id: id,
       title: title,
       postedBy: url,
       url: url,
       timeStamp: getUnixTime(new Date()),
-      upvotes: counter.id
-    }
-    const cid = await this.links.add(link)
+      upvotes: counter.id,
+    };
+    const cid = await this.links.add(link);
     return cid;
   }
-    
+
   async upvoteLink(walletAddress, linkId) {
     const profile = await this.profiles.get(walletAddress);
     const link = await this.links.get(id);
-    const counter = await this.orbitdb.counter(link.counter, this.defaultOptions);
-    await counter.load()
-    const cid = await counter.inc()
+    const counter = await this.orbitdb.counter(
+      link.counter,
+      this.defaultOptions
+    );
+    await counter.load();
+    const cid = await counter.inc();
     const link = {
       id: uuidv4(),
       upvotedBy: profile.id,
       timeStamp: getUnixTime(new Date()),
-      link: link.id
+      link: link.id,
     };
     await this.upvotes.put(link);
     return link;
@@ -99,7 +115,7 @@ export default class DaoCampDb {
   }
 
   async getLinks() {
-    return await this.links.query(q => q.orderBy('timeStamp', 'desc'));
+    return await this.links.query((q) => q.orderBy("timeStamp", "desc"));
   }
 
   async getLink(linkId) {
@@ -107,27 +123,23 @@ export default class DaoCampDb {
   }
 
   async getLinkUpvotes(linkId) {
-    return await this.upvotes.query(q => q.link === linkId);
+    return await this.upvotes.query((q) => q.link === linkId);
   }
 
   async getUpvotedLinks(walletAddress) {
     const profile = await this.profiles.get(walletAddress);
-    return await this.upvotes.query(q => q.upvotedBy === profile.id);
+    return await this.upvotes.query((q) => q.upvotedBy === profile.id);
   }
   async getSubmittedLinks(walletAddress) {
     const profile = await this.profiles.get(walletAddress);
-    return await this.links.query(q => q.postedBy === profile.id);
+    return await this.links.query((q) => q.postedBy === profile.id);
   }
-
-
 
   // async getUpvotesReceived(profileId) {}
   // async getLinksSubmitted(profileId) {}
 
-
-
   async getLinkUpvotes(linkId) {
-    const upvotes = await this.upvotes.query(q => q.link === linkId);
+    const upvotes = await this.upvotes.query((q) => q.link === linkId);
     return upvotes;
   }
 
@@ -145,18 +157,17 @@ export default class DaoCampDb {
     const upvote = await this.upvotes.get(upvoteId);
     return upvote.link;
   }
-  
+}
+
 try {
-    const Ipfs = require("ipfs");
-    const OrbitDB = require("orbit-db");
-    const DCDB = new DaoCampDb(Ipfs, OrbitDB);
-    DCDB.onready = () => {
-      console.log(DCDB.orbitdb.id);
-    }
+  const Ipfs = require("ipfs");
+  const OrbitDB = require("orbit-db");
+  const DCDB = new DaoCampDb(Ipfs, OrbitDB);
+  DCDB.onready = () => {
+    console.log(DCDB.orbitdb.id);
+  };
   DCDB.create();
-    
 } catch (e) {
   console.log(e);
   window.DCDB = new DaoCampDb(window.Ipfs, window.OrbitDB);
 }
-
