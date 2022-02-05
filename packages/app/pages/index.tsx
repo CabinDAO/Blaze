461a52c74777c38b7f0be68b71a27265cc048348
Matthew Watman <mattwatman@gmail.com>
2022-02-04 22:16:58 -0500

    fix(supabase-migration): add supabase queries

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -1,17 +1,16 @@
-import type {NextPage} from "next";
-import {useState, useEffect, useMemo} from "react";
+import type { NextPage } from "next";
+import { useState, useEffect, useMemo } from "react";
 import PostList from "@/components/PostList";
-import {useWallet} from "@/components/WalletAuth";
-import {useStore} from "@/store/store";
+import { useWallet } from "@/components/WalletAuth";
+import { useStore } from "@/store/store";
 import Title from "@/components/Title";
 import ClientSide from "@/components/HOC/ClientSide";
 import Profile from "@/components/Profile";
 import StickyTabBar from "@/components/TabBar";
-import { setupThreadClient, auth } from "@/lib/db";
-import { ThreadID } from "@textile/hub";
+import supabase from "@/lib/supabaseClient";
 
 const Home: NextPage = () => {
-  const {posts, sort } = useStore();
+  const { posts, sort } = useStore();
   const [activeTab, setActiveTab] = useState(0);
   const [scrolled, setScrolled] = useState(false);
 
@@ -33,32 +32,25 @@ const Home: NextPage = () => {
   });
   return (
     <ClientSide>
-      <Profile/>
+      <Profile />
       <Title>Today</Title>
-      <StickyTabBar position={scrolled ? "sticky" : "fixed"}/>
+      <StickyTabBar position={scrolled ? "sticky" : "fixed"} />
       <PostList posts={posts} sort={sort} />
     </ClientSide>
   );
 };
 
-export default Home;  
+export default Home;
 
 export async function getStaticProps() {
-  const userAuth = await auth({
-    key: process.env.TEXTILE_API_KEY || "",
-    secret: process.env.TEXTILE_API_SECRET || "",
-  });
-  const client = await setupThreadClient(userAuth);
-  const threadList = await client.listDBs();
-  const threadId = ThreadID.fromString(threadList[0].id);
-  const posts = await client.find(threadId, "links", {});
-  const upvotes = await client.find(threadId, "upvotes", {});
+  const posts = await supabase.from("post").select();
+  const upvotes = await supabase.from("upvotes").select();
   return {
     props: {
       initialZustandState: {
         posts,
         upvotes,
-     }
+      }
     },
   };
 }
