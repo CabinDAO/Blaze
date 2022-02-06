1e83ec8ffa656b4b88c25ad550528b96f05b122f
Matthew Watman <mattwatman@gmail.com>
2022-02-06 17:04:59 -0500

    fix(supabase-migration): remove client options

--- packages/app/lib/supabaseClient.ts
+++ packages/app/lib/supabaseClient.ts
@@ -1,12 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const options = {
-  schema: 'public',
-  autoRefreshToken: true,
-  persistSession: true,
-  detectSessionInUrl: true
-}
+
 const supabaseId = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
 
-const supabase = createClient("https://sragkqfrlnmzcunfyrqc.supabase.co", supabaseId, options)
+const supabase = createClient("https://sragkqfrlnmzcunfyrqc.supabase.co", supabaseId);
 export default supabase;
