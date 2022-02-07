521d86cd01db96977b34c653f68c73d6492979f0
Matthew Watman <mattwatman@gmail.com>
2022-02-07 15:44:20 -0500

    fix(supabase-client): override default options

--- packages/app/lib/supabaseClient.ts
+++ packages/app/lib/supabaseClient.ts
@@ -4,5 +4,10 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseId = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 
-const supabase = createClient(supabaseUrl, supabaseId);
+const supabase = createClient(supabaseUrl, supabaseId, {
+    schema: 'public',
+    autoRefreshToken: false,
+    persistSession: false,
+    detectSessionInUrl: false,
+});
 export default supabase;
