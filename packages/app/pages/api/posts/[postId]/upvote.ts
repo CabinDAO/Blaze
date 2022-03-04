865f07d4703bacb30a522cbaec46e93d404850ef
Matthew Watman <mattwatman@gmail.com>
2022-03-04 15:53:28 -0500

    feat(SIWE): add authetication check to upvote handler

--- packages/app/pages/api/posts/[postId]/upvote.ts
+++ packages/app/pages/api/posts/[postId]/upvote.ts
@@ -1,19 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import supabase from "@/lib/supabaseClient";
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { ironOptions } from '@/constants';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
-    const { error } = await supabase.rpc("upordownvote", {
-      post_id: req.body.postId,
-      address: req.body.upvoter,
-    });
-
-    if (error) {
-      return res.status(409).json({ message: "Already upvoted" });
-    }
-
-    return res.status(201).json({ postId: req.body.postId });
-  } else {
-    return res.setHeader("ALLOWED", "POST").status(405);
+const handler = async (req: any, res: any) => {
+  const { method } = req;
+  switch (method) {
+    case 'POST':
+      try {
+        if (req.body.siwe){
+           await supabase.rpc("upordownvote", {
+            post_id: req.body.postId,
+            address: req.body.siwe.address,
+          });
+          return res.status(201).json({ postId: req.body.postId });
+        } else {
+          return res.status(403).json({ message: "Action forbidden" });
+        }
+      } catch (_error) {
+        return res.status(409).json({ message: "Already upvoted" });
+      }
+      break;
+    default:
+      return res.setHeader("ALLOWED", "POST").status(405);
   }
 }
+
+export default withIronSessionApiRoute(handler, ironOptions);
