4a9a670db3969bcc113b7c55c8d1e01a9f681b59
Matthew Watman <mattwatman@gmail.com>
2022-04-15 10:00:35 -0400

    feat(comments): split comment interface to match supabase tables

--- packages/app/interfaces.ts
+++ packages/app/interfaces.ts
@@ -8,7 +8,18 @@ export interface IPost {
   upvotes: number;
 }
 
-export interface IComment {
+export interface IPostComment {
+  _id: string;
+  postId: string;
+  text: string;
+  postedBy: string;
+  created_at: string;
+  comments?: ICommentComment[];
+  upvotes: number;
+  upvoted?: boolean;
+}
+
+export interface ICommentComment {
   _id: string;
   text: string;
   postedBy: string;
